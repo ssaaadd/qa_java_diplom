dee50fc0b98869947786601134970e75ec7d4945
Aleksandr Muravev <muravevkot@gmail.com>
2022-11-27 16:20:39 +0300

    add Auth Test to orderCreate tests

--- diplom2/src/test/java/qa_java/OrderTest.java
+++ diplom2/src/test/java/qa_java/OrderTest.java
@@ -1,12 +1,16 @@
 package qa_java;
 
 import io.restassured.response.ValidatableResponse;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import qa_java.client.Client;
 import qa_java.client.OrderClient;
+import qa_java.client.UserClient;
 import qa_java.generators.OrderGenerator;
 import qa_java.model.Order;
+import qa_java.model.User;
 
 import static org.apache.http.HttpStatus.SC_OK;
 
@@ -14,37 +18,51 @@ import static org.apache.http.HttpStatus.SC_OK;
 public class OrderTest extends BaseTest {
     private OrderClient orderClient;
     private Order order;
+    private UserClient userClient;
+    private User userDefault;
+    private String accessToken;
 
 
     @Before
     public void setUp() {
+        //        Создаем шаблоны заказов
         orderClient = new OrderClient();
         order = OrderGenerator.getDefault();
+        //        Для создания/удаления пользователя
+        userClient = new UserClient();
+    }
+
+    @After
+    public void cleanUp() {
+//        Удаляем созданного пользователя
+        accessToken = Client.getAccessToken();
+        if (accessToken != null) {
+            userClient.deleteUser(accessToken);
+        }
     }
 
 
     @Test
-    public void createOrder_NoAuth_Ok_Test() {
+    public void createOrder_NoAuth_Ok() {
         ValidatableResponse responseCreate = orderClient.createOrder(order);
         responseCreate.log().all();
 
         trackNumber = trackExtract(responseCreate);
         statusCodeActual = getStatusCodeActual(responseCreate);
 
-        Assert.assertNotNull("Список заказов пуст", trackNumber);
+        Assert.assertTrue("Список заказов пуст", trackNumber > 0);
         Assert.assertEquals("Статус код неверный", SC_OK, statusCodeActual);
 
     }
 
     @Test
-    public void createOrder_Auth_Ok_Test() {
+    public void createOrder_Auth_Ok() {
         ValidatableResponse responseCreate = orderClient.createOrderAuth(order);
         responseCreate.log().all();
 
         trackNumber = trackExtract(responseCreate);
         statusCodeActual = getStatusCodeActual(responseCreate);
-
-        Assert.assertNotNull("Список заказов пуст", trackNumber);
+        Assert.assertTrue("Список заказов пуст", trackNumber > 0);
         Assert.assertEquals("Статус код неверный", SC_OK, statusCodeActual);
 
     }
